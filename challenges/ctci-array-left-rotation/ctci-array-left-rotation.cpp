b0458d17c963455ee49745814a3ac25ff752a0d6
YuJin Kim <yujinkim@mymail.mines.edu>
2020-06-30 14:34:44 -0700

    Add project

---
+++ challenges/ctci-array-left-rotation/ctci-array-left-rotation.cpp
@@ -0,0 +1,12 @@
+﻿// ctci-array-left-rotation.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
