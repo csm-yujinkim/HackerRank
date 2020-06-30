f4c8a2765866fc65119dd3abb22511a45c4facaf
YuJin Kim <yujinkim@mymail.mines.edu>
2020-06-30 15:00:36 -0700

    Solve "ctci-array-left-rotation"

--- challenges/ctci-array-left-rotation/ctci-array-left-rotation.cpp
+++ challenges/ctci-array-left-rotation/ctci-array-left-rotation.cpp
@@ -2,11 +2,28 @@
 //
 
 #include <iostream>
-
-using namespace std;
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	size_t n, d;
+	std::cin >> n >> d;
+	std::vector<unsigned> xs;
+	xs.reserve(n * 2ul);
+	for (size_t i = 0ul; i < n; ++i) {
+		unsigned x;
+		std::cin >> x;
+		xs.push_back(x);
+	}
+
+	// Copy and slide algorithm
+	std::copy_n(xs.begin(), xs.size(), std::back_inserter(xs));
+	size_t const begin = d + 0ul;
+	size_t const end = d + n;
+	for (size_t i = begin; i < end; ++i) {
+		std::cout << xs.at(i) << ' ';
+	}
 	return 0;
 }
